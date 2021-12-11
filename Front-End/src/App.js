b65786a71ae3ea09190409daa6d9d06e9fe1f7e8
amalmahmoud <amalmahmoud168@gmail.com>
2021-12-11 11:49:44 +0200

    added signin&signup interface

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -7,13 +7,15 @@ import {
 
 import "./App.css";
 import Home from "./views/home";
-
 function App() {
 	return (
 		<Router>
 			{/* All the routes of the application is defined below */}
 
 			<div className="App">
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+				</Routes>
 				<Routes>
 					<Route exact path="/" element={<Home />} />
 				</Routes>
