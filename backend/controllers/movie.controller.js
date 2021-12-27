b1bc654e1037f555e1533d5744dbf1330dde226b
Aya Elsackaan <ayaelsackaan.1999@gmail.com>
2021-12-27 18:37:24 +0200

    create movie

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -3,14 +3,47 @@ const router = express.Router();
 const Joi = require('joi');
 const validateRequest = require('_middleware/validate-request');
 const authorizeAdmin = require('_middleware/authorizeAdmin')
+const authorizeManager = require('_middleware/authorizeManager')
 const movieService = require('services/movie.service');
 const movie_ticketService = require('services/movie-ticket.service');
 // routes
 router.get('/getmovies',  getMovies);
+router.get('/getallmovies',  getAllMovies);
+router.post('/movies/new', authorizeManager, CreateSchema, createMovie);
 module.exports = router;
+
+function CreateSchema(req, res, next) {
+    const schema = Joi.object({
+        title: Joi.string().required(),
+        date: Joi.date().required(),
+        start_time:  Joi.date().timestamp().required(),
+        end_time:Joi.date().timestamp().required(),
+        room: Joi.string().required(),
+        empty_seats_count: Joi.number().required(),
+        price: Joi.number().required(),
+        poster_url: Joi.string().uri(),
+        trailer_url: Joi.string().uri(),
+        
+    });
+    validateRequest(req, next, schema);
+}
+
 
 function getMovies(req, res, next) {
     movie_ticketService.getTicketsOfMovies()
         .then(movies => res.json(movies))
         .catch(next);
+}
+
+function getAllMovies(req, res, next) {
+    movieService.getMovies()
+        .then(movies => res.json(movies))
+        .catch(next);
+}
+
+function createMovie(req, res, next) {
+	movieService
+		.create(req.body)
+		.then(() => res.json({ message: "Creation successful" }))
+		.catch(next);
 }
--- backend/models/movie.model.js
+++ backend/models/movie.model.js
@@ -4,15 +4,15 @@ module.exports = model;
 
 function model(sequelize) {
     const attributes = {
-        title: { type: DataTypes.STRING, allowNull: false},
-        date: {type: DataTypes.DATE, allowNull: false},
-        start_time:{type: DataTypes.TIME, allowNull: false},
+        title: { type: DataTypes.STRING, allowNull: false, unique:'compositeIndex'},
+        date: {type: DataTypes.DATE, allowNull: false, unique:'compositeIndex'},
+        start_time:{type: DataTypes.TIME, allowNull: false, unique:'compositeIndex'},
         end_time: {type: DataTypes.TIME, allowNull: false}, 
-        room: {type: DataTypes.STRING, allowNull: false},
+        room: {type: DataTypes.STRING, allowNull: false, unique:'compositeIndex'},
         empty_seats_count: {type: DataTypes.INTEGER, allowNull: false},
+        price: {type: DataTypes.INTEGER, allowNull: false},
         poster_url: {type: DataTypes.STRING, allowNull: false, defaultValue: "https://image.flaticon.com/icons/png/512/2583/2583338.png"},
         trailer_url: {type: DataTypes.STRING, allowNull: false, defaultValue: "https://image.flaticon.com/icons/png/512/2583/2583338.png"},
-        created_by: { type: DataTypes.STRING, allowNull: true,references:  {model: 'users',key: 'username'}},
     };
 
     Movie=sequelize.define('Movie', attributes);
