ca207a6885b0053c3d76a7d00ab1500afe617066
Aya-Mahmoud-99 <ayamahmoudabdelfatah99@gmail.com>
2022-01-06 16:30:31 +0200

    reservation conflict trial

--- backend/models/ticket.model.js
+++ backend/models/ticket.model.js
@@ -4,7 +4,7 @@ module.exports = model;
 
 function model(sequelize) {
     const attributes = {
-        username: { type: DataTypes.STRING, allowNull: false,primaryKey: true,references: {model: 'users',key: 'username'} },
+        username: { type: DataTypes.STRING, allowNull: false,primaryKey: false,references: {model: 'users',key: 'username'} },
         movie_id: { type: DataTypes.INTEGER, allowNull: false ,primaryKey: true,references: {model: 'movies',key: 'id'}},
         seat_number: { type: DataTypes.INTEGER, allowNull: false ,primaryKey: true}
         };
